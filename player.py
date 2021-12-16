1d97e6cd6cb73d09355239187e5f401043b245d9
themanh01 <nguyenthemanh_t64@hus.edu.vn>
2021-12-16 13:07:54 +0700

    fix boat direction

--- player.py
+++ player.py
@@ -25,36 +25,38 @@ class Player(Entity):
         angle = self.rotation_z
         changeX = 0
         changeY = 0
-        if held_keys['left arrow'] or held_keys['a'] :
-            changeX = -self.speed
-            angle = 90
-        if held_keys['right arrow'] or held_keys['d'] :
-            changeX = self.speed
-            angle = -90
         if held_keys['up arrow'] or held_keys['w']:
             changeY = self.speed
             angle = 180
+    
         if held_keys['down arrow'] or held_keys['s']:
             changeY = -self.speed
             angle = 0
-        if (held_keys['left arrow'] or held_keys['a']) and (held_keys['up arrow'] or held_keys['w']):
-            changeX = -self.speed /1.414
-            changeY = self.speed /1.414
-            angle = -45
-        if (held_keys['right arrow'] or held_keys['d']) and (held_keys['up arrow'] or held_keys['w']):
-            changeX = self.speed /1.414
-            changeY = self.speed /1.414
-            angle = 45
-        if (held_keys['left arrow'] or held_keys['a']) and (held_keys['down arrow'] or held_keys['s']):
-            changeX = -self.speed/1.414
-            changeY = -self.speed /1.414
-            angle = -135
+        if held_keys['left arrow'] or held_keys['a'] :
+            changeX = -self.speed
+            angle = 90
+            if held_keys['up arrow'] or held_keys['w']:
+                angle = 135
+                changeX = -self.speed /1.414
+                changeY = self.speed /1.414
+            elif held_keys['down arrow'] or held_keys['s']:
+                angle = 45
+                changeX = -self.speed/1.414
+                changeY = -self.speed /1.414
 
-        if (held_keys['right arrow'] or held_keys['d']) and (held_keys['down arrow'] or held_keys['s']):
-            changeX = self.speed /1.414
-            changeY = -self.speed /1.414
-            angle = 135
+        if held_keys['right arrow'] or held_keys['d'] :
+            changeX = self.speed
+            angle = -90
+            if held_keys['up arrow'] or held_keys['w']:
+                angle = -135
+                changeX = self.speed /1.414
+                changeY = self.speed /1.414
 
+            elif held_keys['down arrow'] or held_keys['s']:
+                angle = -45
+                changeX = self.speed /1.414
+                changeY = -self.speed /1.414
+        
         self.rotation_z = angle
         
         camera.x = self.x
